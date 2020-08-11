fa1d388f235028fa42328c3f07e466caf23e941a
Kilian Käslin <kilian.kaeslin@quartett-mobile.de>
2020-08-11 15:32:46 +0200

    - Improved DevOps

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,15 +1,25 @@
 const path = require('path');
 const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: 'development',
   watch: true,
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: __dirname,
+    contentBase: path.resolve(__dirname, '.'),
     compress: true,
     hot: true,
     port: 9000,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 });
